3babd6ff64ea62f9658d1612128a222afa7773db
emilkjuka <emilkjuka@gmail.com>
2021-11-12 13:46:35 +0100

    Adding Cars from csv to db and adding 1 dealership
    -The models have been registered
    -Admin site made with superuser
    -Car db uploader
    -1 dealership added manually for testing

--- Domashno 1/base/cardealership/models.py
+++ Domashno 1/base/cardealership/models.py
@@ -13,16 +13,15 @@ class Dealership(models.Model):
         return f"{self.dealership_name} x:{self.dealership_x_coordinate} y:{self.dealership_y_coordinate}"
        
 class Car(models.Model):
-    New = 'New'
-    Used = 'Used'
-    car_model = models.CharField(max_length=50)
+    car_id = models.IntegerField(default=-1)
     car_brand = models.CharField(max_length=50)
-    car_production_year = models.DateField(max_length=50)
+    car_model = models.CharField(max_length=50)
+    car_production_year = models.CharField(max_length=10)
     car_type = models.CharField(max_length=50)
+    car_color = models.CharField(default= "",max_length=50)
     car_price = models.CharField(max_length=50)
-    car_color = models.CharField(default= "",max_length=50, null=True)
     car_state = models.CharField(max_length=50, choices=[('New','New'), ['Used', 'Used']])
-    DealershipFK = models.ForeignKey(Dealership, on_delete=models.CASCADE)
+    DealershipFK = models.IntegerField(default = 1)
     
     def __str__(self):
         return f"{self.car_brand} {self.car_model} {self.car_color}"
