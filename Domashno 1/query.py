5982e934e452da7afb04b5788ea4a35348c09969
Sazzy0 <andrej.sazdovski@gmail.com>
2021-11-13 15:31:09 +0100

    Update query.py

--- Domashno 1/query.py
+++ Domashno 1/query.py
@@ -9,17 +9,13 @@ area[name="Скопје"];
   way["shop"="car"](area);
   node["shop"="car"](area);
 );
-out body;
->;
-out skel qt;
+out center;
+
 """
-response = requests.get(overpass_url, 
+response = requests.get(overpass_url,
                         params={'data': overpass_query})
 
 data = response.json()
 
 with open('dealership_data.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
-
-
-
