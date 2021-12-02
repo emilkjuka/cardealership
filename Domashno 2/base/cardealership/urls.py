d78befb82def7b399318261ac4d3ed13d524a664
emilkjuka <emilkjuka@gmail.com>
2021-12-02 10:39:46 +0100

    commit

--- Domashno 2/base/cardealership/urls.py
+++ Domashno 2/base/cardealership/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home', views.index, name='index'),
+    path('cars', views.cars, name='cars'),
+    path('cars/<int:car_id>', views.cars, name='car_details'),
+    path('dealerships', views.dealership, name='dealerships'),
+    path('contact', views.contact, name='contact'),
+    path('aboutus', views.aboutus, name='aboutus'),
 ]
--- Domashno 2/base/cardealership/views.py
+++ Domashno 2/base/cardealership/views.py
@@ -7,3 +7,17 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to our car dealership app")
+
+def aboutus(request):
+    return HttpResponse("About us")
+
+def contact(request):
+    return HttpResponse("Contact page")
+
+def cars(request):
+    return HttpResponse("Car sort")
+
+def dealership(request):
+    return HttpResponse("Dealership Sort")
+
+
