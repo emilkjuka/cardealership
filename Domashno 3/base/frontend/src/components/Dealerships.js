6bac9012b6c6bcae2dddbc071a23e53f417ab1d3
emilkjuka <emilkjuka@gmail.com>
2021-12-17 15:11:46 +0100

    adding markers to map

--- Domashno 3/base/frontend/src/components/Dealerships.js
+++ Domashno 3/base/frontend/src/components/Dealerships.js
@@ -4,14 +4,17 @@ import Map from "./Map";
 export default class Dealerships extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      cords: [[41.9787012, 21.4478089], [41.9922072, 21.4754564]],
+    };
   }
-
   render() {
     return (
       <div className="dealership_wrapper">
         <div className="dealership_list"></div>
         <div className="dealership_map_wrapper">
-          <Map />
+          {console.log(this.state.cords)}
+          <Map cords={this.state.cords} />
         </div>
       </div>
     );
--- Domashno 3/base/frontend/src/components/Map.js
+++ Domashno 3/base/frontend/src/components/Map.js
@@ -8,10 +8,14 @@ import {
 
 const MyMapComponent = withScriptjs(
   withGoogleMap((props) => (
-    <GoogleMap defaultZoom={12} defaultCenter={{ lat:41.980, lng:21.437802}}>
-      {props.isMarkerShown && (
-        <Marker position={{  lat:41.996387, lng:21.437802}} />
+    <GoogleMap defaultZoom={12} defaultCenter={{ lat: 41.98, lng: 21.437802 }}>
+            {props.isMarkerShown && (
+        <Marker position={{ lat: 41.996387, lng: 21.437802 }} />
       )}
+      {props.cords.map(cords => (
+        console.log(cords),
+        <Marker position={{ lat: cords[0], lng: cords[1] }} />
+      ))}
     </GoogleMap>
   ))
 );
@@ -19,16 +23,18 @@ export default class Map extends Component {
   constructor(props) {
     super(props);
   }
+
   // 41.996387, 21.437802
   render() {
     return (
-      <MyMapComponent
-        isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{height:'100%'}}/>}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+        <MyMapComponent
+          isMarkerShown
+          cords = {this.props.cords}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
     );
   }
 }
